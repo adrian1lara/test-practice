d5087b1c77646001775b471662c7a20158080693
adrian1lara <adrianlara203@gmail.com>
2023-09-14 18:47:43 -0500

    update second test

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -5,6 +5,6 @@ test('capitalize a string', () => {
     expect(capitalize('abc')).toBe('Abc');   
 });
 
-test('capitalize a string', () => {
+test('capitalize cAAADa string to Caaada', () => {
     expect(capitalize('cAAADa')).toBe('Caaada');
 })
